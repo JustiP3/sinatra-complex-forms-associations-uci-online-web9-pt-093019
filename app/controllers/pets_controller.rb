5f405790f818d46b5857be4cdd131f714fe488b2
Justin Phillips <justinphil02@yahoo.com>
2020-02-03 04:23:00 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(:name => params[:name])
+    @pet = Pet.create(:name => params[:pet_name])
     redirect to "pets/#{@pet.id}"
   end
 
